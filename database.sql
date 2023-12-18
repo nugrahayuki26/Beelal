1e4cc4ac1e0c4b18b1f2496c27dffae471e92341
Ardi Renaldi <renaldimedia@gmail.com>
2023-12-19 00:01:23 +0700

    change database.sql pisahkan antara orang dan pelanggan

--- database.sql
+++ database.sql
@@ -94,13 +94,12 @@ CREATE TABLE `m_keahlian` (
 ) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
 
 -- CreateTable
-CREATE TABLE `pelanggan` (
+CREATE TABLE `m_orang` (
     `id` INTEGER NOT NULL AUTO_INCREMENT,
     `nama_lengkap` VARCHAR(191) NOT NULL,
-    `kode_pelanggan` VARCHAR(191) NOT NULL,
     `nik` VARCHAR(191) NULL,
     `jk` CHAR(2) NOT NULL,
-    `tempat_lahir` VARCHAR(191) NULL,
+    `tempat_lahir_id` INTEGER NULL,
     `tgl_lahir` DATETIME(3) NULL,
     `gol_darah` CHAR(2) NULL,
     `id_provinsi` INTEGER NULL,
@@ -117,21 +116,40 @@ CREATE TABLE `pelanggan` (
     `pend_jurusan` VARCHAR(191) NULL,
     `pend_tahun_lulus` VARCHAR(191) NULL,
     `pend_keterangan` VARCHAR(191) NULL,
-    `grade_terakhir` VARCHAR(191) NULL,
-    `tahun_join` INTEGER NULL,
     `pkr_id_pekerjaan` INTEGER NULL,
     `penghasilan_ratarata` INTEGER NULL,
     `id_keahlian` INTEGER NULL,
     `id_tlb` INTEGER NULL,
+    `no_wa` VARCHAR(191) NULL,
+    `email` VARCHAR(191) NULL,
+    `facebook` VARCHAR(191) NULL,
+    `keterangan` TEXT NULL,
+
+    UNIQUE INDEX `m_orang_nik_key`(`nik`),
+    UNIQUE INDEX `m_orang_no_wa_key`(`no_wa`),
+    UNIQUE INDEX `m_orang_email_key`(`email`),
+    UNIQUE INDEX `m_orang_facebook_key`(`facebook`),
+    PRIMARY KEY (`id`)
+) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+
+-- CreateTable
+CREATE TABLE `m_pelanggan` (
+    `id` INTEGER NOT NULL AUTO_INCREMENT,
+    `id_orang` INTEGER NOT NULL,
+    `kode_pelanggan` INTEGER NOT NULL,
+    `grade_terakhir` VARCHAR(191) NULL,
+    `tahun_join` INTEGER NULL,
     `bk` TINYINT NULL DEFAULT 0,
     `kk` TINYINT NULL DEFAULT 0,
     `jml_asset_terakhir` INTEGER NULL,
     `id_status_pelanggan` INTEGER NULL,
     `status_marital` VARCHAR(191) NULL,
     `id_posisi_administrasi` INTEGER NULL,
-    `id_kantor` INTEGER NOT NULL,
+    `created_by_user` INTEGER NOT NULL,
+    `created_at` DATETIME(3) NOT NULL DEFAULT CURRENT_TIMESTAMP(3),
 
-    UNIQUE INDEX `pelanggan_nik_key`(`nik`),
+    UNIQUE INDEX `m_pelanggan_id_orang_key`(`id_orang`),
+    UNIQUE INDEX `m_pelanggan_kode_pelanggan_key`(`kode_pelanggan`),
     PRIMARY KEY (`id`)
 ) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
 
@@ -154,66 +172,7 @@ CREATE TABLE `relasi_pelanggan` (
     PRIMARY KEY (`id`)
 ) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
 
--- AddForeignKey
-ALTER TABLE `m_loc_kota` ADD CONSTRAINT `m_loc_kota_id_provinsi_fkey` FOREIGN KEY (`id_provinsi`) REFERENCES `m_loc_provinsi`(`id`) ON DELETE RESTRICT ON UPDATE CASCADE;
-
--- AddForeignKey
-ALTER TABLE `m_loc_kecamatan` ADD CONSTRAINT `m_loc_kecamatan_id_kota_fkey` FOREIGN KEY (`id_kota`) REFERENCES `m_loc_kota`(`id`) ON DELETE RESTRICT ON UPDATE CASCADE;
-
--- AddForeignKey
-ALTER TABLE `m_loc_kelurahan` ADD CONSTRAINT `m_loc_kelurahan_id_kecamatan_fkey` FOREIGN KEY (`id_kecamatan`) REFERENCES `m_loc_kecamatan`(`id`) ON DELETE RESTRICT ON UPDATE CASCADE;
-
--- AddForeignKey
-ALTER TABLE `pelanggan` ADD CONSTRAINT `pelanggan_id_provinsi_fkey` FOREIGN KEY (`id_provinsi`) REFERENCES `m_loc_provinsi`(`id`) ON DELETE SET NULL ON UPDATE CASCADE;
-
--- AddForeignKey
-ALTER TABLE `pelanggan` ADD CONSTRAINT `pelanggan_id_kota_fkey` FOREIGN KEY (`id_kota`) REFERENCES `m_loc_kota`(`id`) ON DELETE SET NULL ON UPDATE CASCADE;
-
--- AddForeignKey
-ALTER TABLE `pelanggan` ADD CONSTRAINT `pelanggan_id_kecamatan_fkey` FOREIGN KEY (`id_kecamatan`) REFERENCES `m_loc_kecamatan`(`id`) ON DELETE SET NULL ON UPDATE CASCADE;
-
--- AddForeignKey
-ALTER TABLE `pelanggan` ADD CONSTRAINT `pelanggan_id_kelurahan_fkey` FOREIGN KEY (`id_kelurahan`) REFERENCES `m_loc_kelurahan`(`id`) ON DELETE SET NULL ON UPDATE CASCADE;
-
--- AddForeignKey
-ALTER TABLE `pelanggan` ADD CONSTRAINT `pelanggan_pkr_id_pekerjaan_fkey` FOREIGN KEY (`pkr_id_pekerjaan`) REFERENCES `m_pekerjaan`(`id`) ON DELETE SET NULL ON UPDATE CASCADE;
-
--- AddForeignKey
-ALTER TABLE `pelanggan` ADD CONSTRAINT `pelanggan_id_keahlian_fkey` FOREIGN KEY (`id_keahlian`) REFERENCES `m_keahlian`(`id`) ON DELETE SET NULL ON UPDATE CASCADE;
-
--- AddForeignKey
-ALTER TABLE `pelanggan` ADD CONSTRAINT `pelanggan_id_tlb_fkey` FOREIGN KEY (`id_tlb`) REFERENCES `m_tlb`(`id`) ON DELETE SET NULL ON UPDATE CASCADE;
-
--- AddForeignKey
-ALTER TABLE `pelanggan` ADD CONSTRAINT `pelanggan_id_status_pelanggan_fkey` FOREIGN KEY (`id_status_pelanggan`) REFERENCES `m_status_pelanggan`(`id`) ON DELETE SET NULL ON UPDATE CASCADE;
-
--- AddForeignKey
-ALTER TABLE `pelanggan` ADD CONSTRAINT `pelanggan_id_posisi_administrasi_fkey` FOREIGN KEY (`id_posisi_administrasi`) REFERENCES `m_pos_administrasi`(`id`) ON DELETE SET NULL ON UPDATE CASCADE;
-
--- AddForeignKey
-ALTER TABLE `pelanggan` ADD CONSTRAINT `pelanggan_id_kantor_fkey` FOREIGN KEY (`id_kantor`) REFERENCES `m_kantor`(`id`) ON DELETE RESTRICT ON UPDATE CASCADE;
-
--- AddForeignKey
-ALTER TABLE `relasi_pelanggan` ADD CONSTRAINT `relasi_pelanggan_id_dari_pelanggan_fkey` FOREIGN KEY (`id_dari_pelanggan`) REFERENCES `pelanggan`(`id`) ON DELETE RESTRICT ON UPDATE CASCADE;
-
--- AddForeignKey
-ALTER TABLE `relasi_pelanggan` ADD CONSTRAINT `relasi_pelanggan_id_ke_pelanggan_fkey` FOREIGN KEY (`id_ke_pelanggan`) REFERENCES `pelanggan`(`id`) ON DELETE RESTRICT ON UPDATE CASCADE;
-
--- AddForeignKey
-ALTER TABLE `relasi_pelanggan` ADD CONSTRAINT `relasi_pelanggan_id_relasi_fkey` FOREIGN KEY (`id_relasi`) REFERENCES `m_relasi_pel`(`id`) ON DELETE RESTRICT ON UPDATE CASCADE;
-
-/*
-  Warnings:
-
-  - Added the required column `created_by_user` to the `pelanggan` table without a default value. This is not possible if the table is not empty.
-
-*/
--- AlterTable
-ALTER TABLE `pelanggan` ADD COLUMN `created_at` DATETIME(3) NOT NULL DEFAULT CURRENT_TIMESTAMP(3),
-    ADD COLUMN `created_by_user` INTEGER NOT NULL;
-
 -- CreateTable
--- history grade pelanggan
 CREATE TABLE `h_grade_pelanggan` (
     `id` INTEGER NOT NULL AUTO_INCREMENT,
     `id_pelanggan` INTEGER NOT NULL,
@@ -225,7 +184,6 @@ CREATE TABLE `h_grade_pelanggan` (
 ) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
 
 -- CreateTable
--- History input/edit data
 CREATE TABLE `h_data` (
     `id` BIGINT NOT NULL AUTO_INCREMENT,
     `id_user` INTEGER NOT NULL,
@@ -238,4 +196,58 @@ CREATE TABLE `h_data` (
 ) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
 
 -- AddForeignKey
-ALTER TABLE `h_grade_pelanggan` ADD CONSTRAINT `h_grade_pelanggan_id_pelanggan_fkey` FOREIGN KEY (`id_pelanggan`) REFERENCES `pelanggan`(`id`) ON DELETE RESTRICT ON UPDATE CASCADE;
+ALTER TABLE `m_loc_kota` ADD CONSTRAINT `m_loc_kota_id_provinsi_fkey` FOREIGN KEY (`id_provinsi`) REFERENCES `m_loc_provinsi`(`id`) ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE `m_loc_kecamatan` ADD CONSTRAINT `m_loc_kecamatan_id_kota_fkey` FOREIGN KEY (`id_kota`) REFERENCES `m_loc_kota`(`id`) ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE `m_loc_kelurahan` ADD CONSTRAINT `m_loc_kelurahan_id_kecamatan_fkey` FOREIGN KEY (`id_kecamatan`) REFERENCES `m_loc_kecamatan`(`id`) ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE `m_orang` ADD CONSTRAINT `m_orang_tempat_lahir_id_fkey` FOREIGN KEY (`tempat_lahir_id`) REFERENCES `m_loc_provinsi`(`id`) ON DELETE SET NULL ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE `m_orang` ADD CONSTRAINT `m_orang_id_provinsi_fkey` FOREIGN KEY (`id_provinsi`) REFERENCES `m_loc_provinsi`(`id`) ON DELETE SET NULL ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE `m_orang` ADD CONSTRAINT `m_orang_id_kota_fkey` FOREIGN KEY (`id_kota`) REFERENCES `m_loc_kota`(`id`) ON DELETE SET NULL ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE `m_orang` ADD CONSTRAINT `m_orang_id_kecamatan_fkey` FOREIGN KEY (`id_kecamatan`) REFERENCES `m_loc_kecamatan`(`id`) ON DELETE SET NULL ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE `m_orang` ADD CONSTRAINT `m_orang_id_kelurahan_fkey` FOREIGN KEY (`id_kelurahan`) REFERENCES `m_loc_kelurahan`(`id`) ON DELETE SET NULL ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE `m_orang` ADD CONSTRAINT `m_orang_pkr_id_pekerjaan_fkey` FOREIGN KEY (`pkr_id_pekerjaan`) REFERENCES `m_pekerjaan`(`id`) ON DELETE SET NULL ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE `m_orang` ADD CONSTRAINT `m_orang_id_keahlian_fkey` FOREIGN KEY (`id_keahlian`) REFERENCES `m_keahlian`(`id`) ON DELETE SET NULL ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE `m_orang` ADD CONSTRAINT `m_orang_id_tlb_fkey` FOREIGN KEY (`id_tlb`) REFERENCES `m_tlb`(`id`) ON DELETE SET NULL ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE `m_pelanggan` ADD CONSTRAINT `m_pelanggan_id_orang_fkey` FOREIGN KEY (`id_orang`) REFERENCES `m_orang`(`id`) ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE `m_pelanggan` ADD CONSTRAINT `m_pelanggan_kode_pelanggan_fkey` FOREIGN KEY (`kode_pelanggan`) REFERENCES `m_kantor`(`id`) ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE `m_pelanggan` ADD CONSTRAINT `m_pelanggan_id_status_pelanggan_fkey` FOREIGN KEY (`id_status_pelanggan`) REFERENCES `m_status_pelanggan`(`id`) ON DELETE SET NULL ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE `m_pelanggan` ADD CONSTRAINT `m_pelanggan_id_posisi_administrasi_fkey` FOREIGN KEY (`id_posisi_administrasi`) REFERENCES `m_pos_administrasi`(`id`) ON DELETE SET NULL ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE `relasi_pelanggan` ADD CONSTRAINT `relasi_pelanggan_id_dari_pelanggan_fkey` FOREIGN KEY (`id_dari_pelanggan`) REFERENCES `m_pelanggan`(`id`) ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE `relasi_pelanggan` ADD CONSTRAINT `relasi_pelanggan_id_ke_pelanggan_fkey` FOREIGN KEY (`id_ke_pelanggan`) REFERENCES `m_pelanggan`(`id`) ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE `relasi_pelanggan` ADD CONSTRAINT `relasi_pelanggan_id_relasi_fkey` FOREIGN KEY (`id_relasi`) REFERENCES `m_relasi_pel`(`id`) ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE `h_grade_pelanggan` ADD CONSTRAINT `h_grade_pelanggan_id_pelanggan_fkey` FOREIGN KEY (`id_pelanggan`) REFERENCES `m_pelanggan`(`id`) ON DELETE RESTRICT ON UPDATE CASCADE;
